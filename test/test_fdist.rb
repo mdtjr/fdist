2c9222cab8da1d0dd419d2789c71a4708989659a
michael d. towle <gametheoretic@gmail.com>
2013-11-08 16:46:55 -0500

    Initial commit to fdist.

---
+++ test/test_fdist.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestFdist < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
