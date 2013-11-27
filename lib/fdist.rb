a96f9a98374769bb94eda91483322d90d8d973e5
michael d. towle <gametheoretic@gmail.com>
2013-11-27 05:13:41 -0500

    add header functionality! (kinda cool)

--- lib/fdist.rb
+++ lib/fdist.rb
@@ -1,9 +1,59 @@
 require 'mathn'
 
+module Headers
+
+	def headers
+		begin
+			[0].keys
+		rescue
+			nil
+		end
+	end
+
+	def headers= param_list
+		if first.class != Hash
+			map! do |val_set|
+				
+
+				hash = {}
+				arrayify(val_set).each_with_index do |elem, idx|
+					hash[param_list[idx += 1]] = elem
+				end
+				hash
+			end
+		end
+		map! do |val_hash|
+			redo_headers(val_hash, param_list)
+		end
+		param_list
+	end
+
+private
+
+	def redo_headers old_hash, new_headers
+		new_hash = {}
+		new_headers.each_with_index do |new_header,idx|
+			new_hash[new_header] = old_hash.values[idx]
+		end
+		new_hash
+	end
+
+	def arrayify thing
+		begin
+			thing.values
+		rescue
+			thing.strip.split ","
+		rescue
+			thing
+		end
+	end
+end
+
+
 module Frequency
 
-	def freq expression
-		Rational.instance_eval("#{count(expression)}/#{length}")
+	def freq &expression
+		Rational.instance_eval("#{count(&expression)}/#{length}")
 	end
 
 	def fdist
@@ -16,4 +66,5 @@ end
 
 class Array
 	include Frequency
+	include Headers
 end
