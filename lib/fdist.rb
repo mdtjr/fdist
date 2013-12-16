d9641822654215c663502c9dcb64184983003c92
michael d. towle <gametheoretic@gmail.com>
2013-12-16 15:36:13 -0500

    overhauled

--- lib/fdist.rb
+++ lib/fdist.rb
@@ -1,83 +1,19 @@
-require 'mathn'
 
-module Headers
-
-	def headers
-		begin
-			[0].keys
-		rescue
-			nil
-		end
-	end
-
-	def headers= param_list
-		if first.class != Hash
-			if first.class != String
-				map! do |val_set|
-					
-
-					hash = {}
-					val_set.each_with_index do |elem, idx|
-						hash[param_list[idx += 1]] = elem
-					end
-					hash
-				end
-			else
-				map! do |val_set|
-					
-
-					hash = {}
-					arrayify(val_set).each_with_index do |elem, idx|
-						hash[param_list[idx += 1]] = elem
-					end
-					hash
-				end
+module Enumerable
+	def set_headers param_list
+		map! do |arr|
+			param_list.zip(arr).inject({}) do |result,zipped|
+				result[zipped.first] = zipped.last
+				result
 			end
 		end
-		map! do |val_hash|
-			redo_headers(val_hash, param_list)
+	end
+
+	def fdist &block
+		temp = self.map &block
+		temp.inject({}) do |result,element|
+			result[element] = temp.count element
+			result
 		end
-		param_list
-	end
-
-private
-
-	def redo_headers old_hash, new_headers
-		new_hash = {}
-		new_headers.each_with_index do |new_header,idx|
-			new_hash[new_header] = old_hash.values[idx]
-		end
-		new_hash
-	end
-
-	def arrayify thing
-		begin
-			thing.values
-		rescue
-			thing.strip.split ","
-		end
-	end
-end
-
-
-module Frequency
-
-	def freq &expression
-		Rational.instance_eval("#{count(&expression)}/#{length}")
-	end
-
-	def fdist
-		memo = {}
-		uniq.sort_by{|x| count(x)}.each do |x|
-			memo[x] = count(x)
-		end
-		memo
-	end
-
-
-end
-
-class Array
-	include Frequency
-	include Headers
+	end	
 end
